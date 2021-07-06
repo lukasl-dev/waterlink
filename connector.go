f60ce8e186d4d4113765a4a1ddcf97443ae4ceb1
lukasl-dev <git@lukasl.dev>
2021-07-07 00:29:29 +0200

    feat: add Connect function

--- connector.go
+++ connector.go
@@ -26,8 +26,82 @@ package waterlink
 
 import (
 	"context"
+	"net/http"
+	"net/url"
+	"strconv"
+
+	"github.com/gorilla/websocket"
+	"github.com/lukasl-dev/waterlink/driver/websocketdriver"
 )
 
-type Connector interface {
-	Open(ctx context.Context) (conn Connection, resumed bool, err error)
+type connector struct {
+	opts *ConnectOptions
+}
+
+func Connect(ctx context.Context, host url.URL, opts ...*ConnectOptions) (Connection, error) {
+	return newConnector(opts).open(ctx, host)
+}
+
+func newConnector(opts []*ConnectOptions) *connector {
+	return &connector{
+		opts: minimizeConnectOptions(opts),
+	}
+}
+
+func (c *connector) open(ctx context.Context, host url.URL) (Connection, error) {
+	conn, resp, err := c.dial(ctx, host)
+	if err != nil {
+		return nil, err
+	}
+	return c.connection(conn, resp), nil
+}
+
+func (c *connector) dial(ctx context.Context, host url.URL) (*websocket.Conn, *http.Response, error) {
+	return c.opts.dialer.DialContext(ctx, host.String(), c.requestHeader())
+}
+
+func (c *connector) requestHeader() http.Header {
+	h := make(http.Header)
+	c.appendAuthHeader(&h)
+	c.appendNumShardsHeader(&h)
+	c.appendUserIDHeader(&h)
+	c.appendResumeKeyHeader(h)
+	return h
+}
+
+func (c *connector) appendAuthHeader(h *http.Header) {
+	h.Set(headerAuthorization, c.opts.passphrase)
+}
+
+func (c *connector) appendNumShardsHeader(h *http.Header) {
+	h.Set(headerNumShards, strconv.Itoa(int(c.opts.numShards)))
+}
+
+func (c *connector) appendUserIDHeader(h *http.Header) {
+	h.Set(headerUserID, strconv.Itoa(int(c.opts.userID)))
+}
+
+func (c *connector) appendResumeKeyHeader(h http.Header) {
+	if len(c.opts.resumeKey) > 0 {
+		h.Set(headerResumeKey, c.opts.resumeKey)
+	}
+}
+
+func (c *connector) connection(conn *websocket.Conn, resp *http.Response) Connection {
+	return &connection{
+		ResumingConfigurer: websocketdriver.NewResumeConfigurer(conn),
+		Destroyer:          websocketdriver.NewDestroyer(conn),
+		Equalizer:          websocketdriver.NewEqualizer(conn),
+		Pauser:             websocketdriver.NewPauser(conn),
+		Player:             websocketdriver.NewPlayer(conn),
+		Seeker:             websocketdriver.NewSeeker(conn),
+		Stopper:            websocketdriver.NewStopper(conn),
+		VoiceUpdater:       websocketdriver.NewVoiceUpdater(conn),
+		VolumeUpdater:      websocketdriver.NewVolumeUpdater(conn),
+		resumed:            c.resumed(resp.Header),
+	}
+}
+
+func (c *connector) resumed(h http.Header) bool {
+	return h.Get(headerSessionResumed) == "true"
 }
