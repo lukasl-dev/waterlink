9dff62222895a3a879b4b28d8c0e056c31b2cdac
lukasl-dev <git@lukasl.dev>
2021-07-07 10:27:54 +0200

    docs(pause usecase): update comment of Pauser.SetPaused method

--- usecase/pause/pause.go
+++ usecase/pause/pause.go
@@ -26,7 +26,7 @@ package pause
 
 // Pauser wraps the SetPaused method.
 type Pauser interface {
-	// SetPaused sets the paused state of an audio player
-	// of a guild to the passed parameter value.
+	// SetPaused sets the paused state of a guild's
+	// audio player to the passed parameter value.
 	SetPaused(guildID uint, paused bool) error
 }
