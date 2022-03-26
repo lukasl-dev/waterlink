4fcb779f56b9048a1c6d73dfee6d7a6b0c3f44f6
lukasl-dev <git@lukasl.dev>
2022-03-26 16:18:02 +0100

    feat(queries): add Of()

--- query/query.go
+++ query/query.go
@@ -5,6 +5,11 @@ import "fmt"
 // Query represents a string used to load audio tracks from the server.
 type Query string
 
+// Of wraps s into a Query. No adjustments are applied on s.
+func Of(s string) Query {
+	return Query(s)
+}
+
 // YouTube returns a query that searches for YouTube videos matching the given
 // search query.
 func YouTube(search string) Query {
