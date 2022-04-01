4f18246e2015ce417495db1e2ba6dc56f23fa525
lukasl-dev <git@lukasl.dev>
2022-04-01 11:41:03 +0200

    feat(connection): add ConfigureResuming() and DisableResuming()

--- connection.go
+++ connection.go
@@ -3,7 +3,10 @@ package waterlink
 import (
 	"github.com/gompus/snowflake"
 	"github.com/gorilla/websocket"
+	"github.com/lukasl-dev/waterlink/internal/message"
+	"github.com/lukasl-dev/waterlink/internal/message/opcode"
 	"github.com/lukasl-dev/waterlink/internal/pkgerror"
+	"time"
 )
 
 // Connection is used to receive and dispatch messages from the
@@ -31,4 +34,27 @@ func newConnection(conn *websocket.Conn) *Connection {
 // availability is not checked client-side.
 func (conn Connection) Guild(id snowflake.Snowflake) Guild {
 	return Guild{w: conn.conn, id: id}
+}
+
+// ConfigureResuming enable the resumption of the session and defines the number
+// of seconds after which the session will be considered expired server-side. This
+// is useful to avoid stopping the audio players that are related to the session.
+func (conn Connection) ConfigureResuming(key string, timeout time.Duration) error {
+	return pkgerror.Wrap("connection: configure resuming", conn.conn.WriteJSON(
+		message.ConfigureResuming{
+			Outgoing: message.Outgoing{Op: opcode.ConfigureResuming},
+			Key:      &key,
+			Timeout:  uint(timeout.Seconds()),
+		},
+	))
+}
+
+// DisableResuming disables the resumption of the session. If disabled, audio
+// players will stop immediately after the connection is closed.
+func (conn Connection) DisableResuming() error {
+	return pkgerror.Wrap("connection: disable resuming", conn.conn.WriteJSON(
+		message.ConfigureResuming{
+			Outgoing: message.Outgoing{Op: opcode.ConfigureResuming},
+		},
+	))
 }
