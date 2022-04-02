215bfd0d525e5ed20d4afc726b31323e34efe6af
lukasl-dev <git@lukasl.dev>
2022-04-03 01:15:56 +0200

    docs: add ExampleNewClient()

---
+++ client_test.go
@@ -0,0 +1,22 @@
+package waterlink
+
+import (
+	"fmt"
+	"log"
+)
+
+func ExampleNewClient() {
+	creds := Credentials{
+		Authorization: "youshallnotpass", // passphrase defined in the server's application.yml
+	}
+
+	cl, err := NewClient("http://localhost:2333", creds)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("Client dispatches actions to %q.\n", cl.url.String())
+
+	// Output:
+	// Client dispatches actions to "http://localhost:2333".
+}
